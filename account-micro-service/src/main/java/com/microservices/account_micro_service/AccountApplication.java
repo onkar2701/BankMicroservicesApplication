5d9d2df43eee0aaebd5caafdae4a9525a4e4bdd7
onkar27 <onkarkadam27@gmail.com>
2024-09-14 13:21:31 +0530

    project setup

---
+++ account-micro-service/src/main/java/com/microservices/account_micro_service/AccountApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.account_micro_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountApplication.class, args);
+	}
+
+}
