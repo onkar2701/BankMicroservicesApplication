e1e449d6ead853c33047ee5aa5a11effc52acc6d
onkar27 <onkarkadam27@gmail.com>
2024-09-17 17:29:01 +0530

    docker files generation for all micro services

---
+++ cards-micro-service/src/test/java/com/microservices/cards_micro_service/CardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.cards_micro_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
